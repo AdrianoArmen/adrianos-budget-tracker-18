070fdf564779e28611bc77ff21ad5400b532448c
AdrianoA <adriano.armendaris@gmail.com>
2021-08-24 01:26:44 -0500

    files formatted

--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-//Schema definition
+//Schema for DB
 const WorkoutSchema = new Schema({
   day: {
     type: Date,
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const apiRoutes = require('./apiR');
-const homeRoutes = require('./homeR');
+const apiRoutes = require("./apiR");
+const homeRoutes = require("./homeR");
 
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router; 
+module.exports = router;
